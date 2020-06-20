0fbf22a01b3c2835fcc2e9d7b2cb69df4763f4cf
diogotito <diogotitomarques@gmail.com>
2020-06-20 18:40:55 +0100

    Show xfce4-appfinder Bookmarks under mouse

--- bin/xfce4-appfinder-category.sh
+++ bin/xfce4-appfinder-category.sh
@@ -3,13 +3,20 @@
 # Opens XFCE4's Application Finder in a given category
 # ("All Applications" by default)
 
+
+# Get or set (with -s/--set) the category in the Application Finder
 appfinder-category() {
-    xfconf-query -c xfce4-appfinder -p /last/category -n -t string "$@"
+    xfconf-query \
+        --channel xfce4-appfinder --property /last/category \
+        --create --type string "$@"
 }
 
-OLD_CAT=$(appfinder-category)
 
-appfinder-category -s "${1:-"All Applications"}"
-xfce4-appfinder
-appfinder-category -s "$OLD_CAT"
+NEW_CAT=${1:-"All Applications"}
+OLD_CAT=$(appfinder-category)
+shift
+
+appfinder-category --set "$NEW_CAT"
+xfce4-appfinder "$@"
+appfinder-category --set "$OLD_CAT"
 
