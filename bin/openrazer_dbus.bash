0d7b6915c072afe6d86934200e0593b0b0b80776
diogotito <diogotitomarques@gmail.com>
2020-10-15 23:07:39 +0100

    Simplify Razer DPI script
    Array expansion in a string was an unnecessary complication

--- bin/openrazer_dbus.bash
+++ bin/openrazer_dbus.bash
@@ -1,14 +1,13 @@
 #!/usr/bin/bash
 
-MOUSENAME=${1:-'Razer DeathAdder Chroma'}
-OPENRAZER=(qdbus org.razer /org/razer)
+DPI=${1:-3000}
+MOUSENAME=${2:-'Razer DeathAdder Chroma'}
 
-# shellcheck disable=SC2145
-# ${OPENRAZER[@]}"/device/<device> calls methods on <device>
-
-for device in $("${OPENRAZER[@]}" getDevices); do
-    if [[ $("${OPENRAZER[@]}"/device/"$device" getDeviceName) == "$MOUSENAME" ]]; then
-        "${OPENRAZER[@]}"/device/"$device" setDPI 3000 3000
+for device in $(qdbus org.razer /org/razer getDevices); do
+    device_name=$(qdbus org.razer /org/razer/device/"$device" getDeviceName)
+    if [[ $device_name == "$MOUSENAME" ]]; then
+        set -x
+        qdbus org.razer /org/razer/device/"$device" setDPI "$DPI" "$DPI"
     fi
 done
 
