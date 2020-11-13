2ffe804cc2b1b3a0f65166412a1f986cbbb6bb13
diogotito <diogotitomarques@gmail.com>
2020-11-13 21:55:14 +0000

    Move inner functions outside of async main
    The code looks clearer now!

--- bin/i3-workspace-starter.py
+++ bin/i3-workspace-starter.py
@@ -3,7 +3,7 @@
 import asyncio
 import pprint
 
-from gi.repository.GLib import GError, Variant
+from gi.repository.GLib import Variant
 from i3ipc import Event
 from i3ipc.aio import Connection
 from pydbus import SessionBus
@@ -21,38 +21,40 @@ WORKSPACE_CATEGORIES = {
     0: "Other"
 }
 
+bus = SessionBus()
+xfconf = bus.get("org.xfce.Xfconf")
 
-# asyncio isn't adding any real value to this script, isn't it?
+
+def xfce4_appfinder_settings(settings):
+    for property, value in settings.items():
+        xfconf.SetProperty("xfce4-appfinder", property, value)
+
+
+async def open_xfce4_appfinder(i3, workspace_name=""):
+    try:
+        bus.get("org.xfce.Appfinder").OpenWindow(True, workspace_name)
+    except:
+        await i3.command("exec xfce4-appfinder")
+
+
+async def on_workspace_focus(i3, event):
+    await i3.command('[class="Xfce4-appfinder"] kill')
+
+    if event.current.focus:
+        return
+
+    if event.current.num in WORKSPACE_CATEGORIES:
+        category = WORKSPACE_CATEGORIES[event.current.num]
+        xfce4_appfinder_settings({
+            "/last/category": Variant("s", category),
+            "/enable-service": Variant("b", True),
+            "/single-window": Variant("b", True),
+        })
+        await open_xfce4_appfinder(i3, event.current.name)
+
+
 async def main():
-    bus = SessionBus()
-    xfconf = bus.get("org.xfce.Xfconf")
     i3 = await Connection(auto_reconnect=True).connect()
-
-    def xfce4_appfinder_settings(settings):
-        for property, value in settings.items():
-            xfconf.SetProperty("xfce4-appfinder", property, value)
-
-    async def open_xfce4_appfinder(workspace_name=""):
-        try:
-            bus.get("org.xfce.Appfinder").OpenWindow(True, workspace_name)
-        except:
-            await i3.command("exec xfce4-appfinder")
-
-    async def on_workspace_focus(_i3, event):
-        await i3.command('[class="Xfce4-appfinder"] kill')
-
-        if event.current.focus:
-            return
-
-        if event.current.num in WORKSPACE_CATEGORIES:
-            category = WORKSPACE_CATEGORIES[event.current.num]
-            xfce4_appfinder_settings({
-                "/last/category": Variant("s", category),
-                "/enable-service": Variant("b", True),
-                "/single-window": Variant("b", True),
-            })
-            await open_xfce4_appfinder(event.current.name)
-
     i3.on(Event.WORKSPACE_FOCUS, on_workspace_focus)
     await i3.main()
 
