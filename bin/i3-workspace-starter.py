806afbe895478fcec4dbf2f82996ea5d67a6c8a6
diogotito <diogotitomarques@gmail.com>
2020-11-18 03:15:51 +0000

    Comment i3-workspace-starter.py

--- bin/i3-workspace-starter.py
+++ bin/i3-workspace-starter.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
 
+"""
+Make your i3 session feel less empty with... XFCE4's Application Finder!
+This script will stick an Application Finder in every new workspace you
+enter. It gets dismissed when it loses focus to get out of your way.
+
+It is assumed that i3 is configured with:
+    focus_follows_mouse yes
+    focus_on_window_activation focus
+"""
+
 import asyncio
-import pprint
 
 from gi.repository.GLib import Variant
 from i3ipc import Event
 from i3ipc.aio import Connection
 from pydbus import SessionBus
 
+# Each numbered workspace is associated with an application category
 WORKSPACE_CATEGORIES = {
     1: "Accesssories",
     2: "Chromium Apps",
@@ -18,9 +28,9 @@ WORKSPACE_CATEGORIES = {
     7: "Internet",
     8: "Settings",
     9: "System",
-    0: "Other"
 }
 
+# Tweak xfce4-appfinder over D-Bus
 bus = SessionBus()
 xfconf = bus.get("org.xfce.Xfconf")
 xfconf.SetProperty("xfce4-appfinder", "/enable-service", Variant("b", True))
@@ -28,22 +38,30 @@ xfconf.SetProperty("xfce4-appfinder", "/single-window", Variant("b", True))
 
 
 async def on_workspace_focus(i3, event):
+    """Show the appfinder when an empty workspace receives focus"""
+
+    # Close any open appfinder window
     await i3.command('[class="Xfce4-appfinder"] kill')
 
+    # Do nothing if this workspace isn't empty
     if event.current.focus:
         return
 
     if event.current.num in WORKSPACE_CATEGORIES:
+        # Switch appfinder category to the one configured for this workspace
         category = Variant("s", WORKSPACE_CATEGORIES[event.current.num])
         xfconf.SetProperty("xfce4-appfinder", "/last/category", category)
+
         try:
-            bus.get("org.xfce.Appfinder").OpenWindow(True, 'i3wspy')
+            # Open a new appfinder window in the new category
+            bus.get("org.xfce.Appfinder").OpenWindow(True, 'such_fast')
         except:
             # The xfce4-appfinder service isn't running. Start it up.
             await i3.command("exec --no-startup-id xfce4-appfinder")
 
 
 async def on_window_focus(i3, event):
+    """Close the appfinder when it loses focus"""
     if event.container.window_class != "Xfce4-appfinder":
         await i3.command('[class="Xfce4-appfinder"] kill')
 
