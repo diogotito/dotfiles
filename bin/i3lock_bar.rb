ab4f897f06534d12190ddb2d86ddde9241accf1e
diogotito <diogotitomarques@gmail.com>
2021-08-06 17:54:37 +0100

    Prefixes and suffixes for the Ruby DSL

--- bin/i3lock_bar.rb
+++ bin/i3lock_bar.rb
@@ -7,6 +7,8 @@ INSIDE_PARENTHESIS = 2
 BOTH = 2
 RIGHT = 2
 
+# DSL
+
 module Flaggable
   def flags
     @flags ||= []
@@ -16,10 +18,14 @@ module Flaggable
     "--"
   end
 
+  def flag_suffix
+    ""
+  end
+
   def pos(x, y)
     x = "w#{x}" if x < 0
     y = "h#{y}" if y < 0
-    flags << "#{flag_prefix}pos=#{x}:#{y}"
+    flags << "#{flag_prefix}pos#{flag_suffix}=#{x}:#{y}"
   end
 
   def rgb(r, g, b, a=255)
@@ -29,9 +35,9 @@ module Flaggable
   def method_missing(flag, value=nil)
     flag = flag.to_s.gsub '_', '-'
     if value.nil?
-      flags << "#{flag_prefix}#{flag}"
+      flags << "#{flag_prefix}#{flag}#{flag_suffix}"
     else
-      flags << "#{flag_prefix}#{flag}=#{value}"
+      flags << "#{flag_prefix}#{flag}#{flag_suffix}=#{value}"
     end
   end
 
@@ -65,8 +71,20 @@ def misc_flags(&block)
   such_dry block
 end
 
+def ending_with what, &block
+  suffixes = [what]
+  such_dry(block) { |m|
+    define_method(:flag_suffix) { suffixes.join }
+    define_method(:_) { |name, &block|
+      suffixes.unshift name
+      instance_eval &block
+      suffixes.shift
+    }
+  }
+end
+
 def flags_for what, &block
-  prefixes = ["--#{what}-"]
+  prefixes = ["--#{what}"]
   such_dry(block) { |m|
     define_method(:flag_prefix) { prefixes.join }
     define_method(:_) { |name, &block|
@@ -77,27 +95,33 @@ def flags_for what, &block
   }
 end
 
-#                                                
-#  Apagar os 2 primeiros argumentos para correr  
-#                                                
 
-exec "printf", "[%s]\\n", "i3lock", *[
+# exec "printf", "[%s]\\n", "i3lock", *[  # dry
+exec "i3lock", *[
+
   misc_flags {
     bar_indicator
     clock
     composite
-    bshl_color '4D586E'
-    keyhl_color '5294E2'
     keylayout FULL
-    layout_color '1793D188'
     redraw_thread
     refresh_rate 0.1
     spaaaaaaaaaace! :blur, 5
   },
-  flags_for('ind') {
+
+  ending_with('-color') {
+    layout '1793D188'
+    _('hl') {
+      bs '4D586E'   # backspace
+      key '5294E2'  # keystroke
+    }
+  },
+
+  flags_for('ind-') {
     pos 683, 75
   },
-  flags_for('bar') {
+
+  flags_for('bar-') {
     base_width 10
     color rgb 0x26, 0x2a, 0x32, 0x77
     direction BOTH
@@ -105,20 +129,24 @@ exec "printf", "[%s]\\n", "i3lock", *[
     pos 0, 10
     step 2
   },
-  flags_for('date') {
+
+  flags_for('date-') {
     color rgb 0x17, 0x93, 0xD1, 0x88
   },
-  flags_for('greeter') {
+
+  flags_for('greeter-') {
     text 'I :heart: Ruby!'
     align RIGHT
     pos -25, -25
   },
+
   flags_for('time') {
-    color '1793D1'
-    _('outline') {
+    _color '1793D1'
+    _('outline-') {
       color '262a3288'
       width 1
     }
   },
+
 ].collect(&:flags).flatten
 
